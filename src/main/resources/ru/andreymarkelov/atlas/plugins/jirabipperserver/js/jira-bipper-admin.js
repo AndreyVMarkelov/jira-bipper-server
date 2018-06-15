5eb284b498675e713a43049b6fbe3ddc6d3b0a37
amarkelov <andrey.markelov@infobip.com>
2018-06-15 13:37:55 +0300

    Init

--- src/main/resources/ru/andreymarkelov/atlas/plugins/jirabipperserver/js/jira-bipper-admin.js
+++ src/main/resources/ru/andreymarkelov/atlas/plugins/jirabipperserver/js/jira-bipper-admin.js
@@ -1,6 +1,23 @@
 AJS.toInit(function () {
     function updateAccountSetup() {
-        alert("1");
+        AJS.$.ajax({
+            url: AJS.contextPath() + "/rest/bipper/1.0/admin",
+            type: "PUT",
+            dataType : "json",
+            contentType: "application/json",
+            data: JSON.stringify({sender: "dd"}),
+            processData: false,
+            success: function () {
+                AJS.dialog2("#bipper-user-admin-dialog").hide();
+                AJS.$("#bipper-action-dialog").removeClass("hidden");
+            },
+            error: function (error) {
+                if (error.responseText) {
+                    AJS.$("#error").text(error.responseText);
+                    AJS.$("#error").show();
+                }
+            }
+        });
     }
 
     AJS.$("#bipper-dialog-save").click(function (e) {
