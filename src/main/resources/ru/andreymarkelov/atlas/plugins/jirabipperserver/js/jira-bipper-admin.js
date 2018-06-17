b57a9154fbb525e1f89a767a5c0108f818f93f55
amarkelov <andrey.markelov@infobip.com>
2018-06-17 16:26:01 +0300

    Init

--- src/main/resources/ru/andreymarkelov/atlas/plugins/jirabipperserver/js/jira-bipper-admin.js
+++ src/main/resources/ru/andreymarkelov/atlas/plugins/jirabipperserver/js/jira-bipper-admin.js
@@ -9,6 +9,9 @@ AJS.toInit(function () {
             processData: false,
             success: function () {
                 AJS.dialog2("#bipper-admin-setup-dialog").hide();
+                AJS.$("#senderName-value").text(AJS.$("#up-bipper-setup-sendername").val());
+                AJS.$("#apiKey-value").text(AJS.$("#up-bipper-setup-apikey").val());
+                AJS.$("#generationTime-block").hide();
 //                AJS.$("#bipper-admin-setup-dialog").removeClass("hidden");
             },
             error: function (error) {
@@ -20,16 +23,19 @@ AJS.toInit(function () {
         });
     }
 
-    function updateAccountSetup1() {
+    function generateKeySetup() {
         AJS.$.ajax({
-            url: AJS.contextPath() + "/rest/bipper/1.0/admin",
+            url: AJS.contextPath() + "/rest/bipper/1.0/admin/generatekey",
             type: "PUT",
             dataType : "json",
             contentType: "application/json",
-            data: JSON.stringify({sender: "dd"}),
+            data: JSON.stringify({sender: AJS.$("#up-bipper-generate-sendername").val(), accountId: AJS.$("#up-bipper-generate-accountid").val(), password: AJS.$("#up-bipper-generate-accountpass").val()}),
             processData: false,
             success: function () {
-                AJS.dialog2("#bipper-user-admin-dialog").hide();
+                AJS.$("#senderName-value").text(AJS.$("#up-bipper-generate-sendername").val());
+                AJS.$("#apiKey-value").text(AJS.$("#up-bipper-generate-sendername").val());
+                AJS.$("#apiKey-value").text(AJS.$("#up-bipper-generate-sendername").val());
+                AJS.dialog2("#bipper-admin-generate-dialog").hide();
                 AJS.$("#bipper-action-dialog").removeClass("hidden");
             },
             error: function (error) {
@@ -41,19 +47,11 @@ AJS.toInit(function () {
         });
     }
 
-    AJS.$("#bipper-dialog-close").click(function (e) {
-        e.preventDefault();
-        AJS.dialog2("#bipper-user-admin-dialog").hide();
-    });
-
-    AJS.$("#edit-bipper-admin-link").click(function(e) {
-        e.preventDefault();
-        AJS.dialog2("#bipper-user-admin-dialog").show();
-    });
-
     // Setup
     AJS.$("#edit-bipper-admin-key-link").click(function(e) {
         e.preventDefault();
+        AJS.$("#up-bipper-setup-sendername").val(AJS.$("#senderName-value").text());
+        AJS.$("#up-bipper-setup-apikey").val(AJS.$("#apiKey-value").text());
         AJS.dialog2("#bipper-admin-setup-dialog").show();
     });
 
@@ -65,5 +63,24 @@ AJS.toInit(function () {
     AJS.$("#bipper-setup-dialog-close").click(function (e) {
         e.preventDefault();
         AJS.dialog2("#bipper-admin-setup-dialog").hide();
+    });
+
+    // Generate
+    AJS.$("#edit-bipper-admin-password-link").click(function(e) {
+        e.preventDefault();
+        AJS.$("#up-bipper-generate-sendername").val(AJS.$("#senderName-value").text());
+        AJS.$("#up-bipper-generate-accountid").val("");
+        AJS.$("#up-bipper-generate-accountpass").val("");
+        AJS.dialog2("#bipper-admin-generate-dialog").show();
+    });
+
+    AJS.$("#bipper-generate-dialog-save").click(function (e) {
+        e.preventDefault();
+        generateKeySetup();
+    });
+
+    AJS.$("#bipper-generate-dialog-close").click(function (e) {
+        e.preventDefault();
+        AJS.dialog2("#bipper-admin-generate-dialog").hide();
     });
 });
